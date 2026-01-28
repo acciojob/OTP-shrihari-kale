3b4c2e9a1b95360b58d4e5ed8031e04887949e48
Shrihari Kale <163184413+shrihari-kale@users.noreply.github.com>
2026-01-28 16:05:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+    codes[0].focus();
+
+    codes.forEach((code, index) => {
+      code.addEventListener('keydown', (e) => {
+
+        if (e.key >= '0' && e.key <= '9') {
+          code.value = '';
+          setTimeout(() => {
+            if (index < codes.length - 1) {
+              codes[index + 1].focus();
+            }
+          }, 10);
+        }
+
+        if (e.key === 'Backspace') {
+          if (code.value === '' && index > 0) {
+            codes[index - 1].focus();
+          }
+        }
+      });
+    });
